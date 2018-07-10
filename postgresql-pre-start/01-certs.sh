75f606768b1f907bd60c9157b2760f74c0f0c6ec
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-10 17:05:50 -0400

    Initial commit.

---
+++ postgresql-pre-start/01-certs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp /tmp/certs/root.crt /var/lib/pgsql/data/root.crt
+cp /tmp/certs/root.crt /etc/pki/ca-trust/source/anchors/
+cp /tmp/certs/server-cert.pem /var/lib/pgsql/data/server.crt
+cp /tmp/certs/server-cert.pem /etc/pki/tls/certs/
+cp /tmp/certs/server-key.pem /var/lib/pgsql/data/server.key
+cp /tmp/certs/server-key.pem /etc/pki/tls/private/
+chmod 600 /var/lib/pgsql/data/server.key /etc/pki/tls/private/server.key
+update-ca-trust enable
+update-ca-trust extract
+
+echo "hostssl all         shah    0.0.0.0/0             md5 clientcert=1" >> /var/lib/pgsql/data/pg_hba.conf
