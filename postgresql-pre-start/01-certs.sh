1539b62898ae5c851f358bb46b578b59dbaa4834
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-14 18:07:58 -0400

    Added init folder.

--- postgresql-pre-start/01-certs.sh
+++ postgresql-pre-start/01-certs.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-cp /tmp/certs/root.crt /var/lib/pgsql/data/root.crt
-# cp /tmp/certs/root.crt /etc/pki/ca-trust/source/anchors/
-cp /tmp/certs/server.crt /var/lib/pgsql/data/server.crt
-#cp /tmp/certs/server-cert.pem /var/lib/pgsql/data/server.crt
-# cp /tmp/certs/server-cert.pem /etc/pki/tls/certs/
-cp /tmp/certs/server.key /var/lib/pgsql/data/server.key
-#cp /tmp/certs/server-key.pem /var/lib/pgsql/data/server.key
-# cp /tmp/certs/server-key.pem /etc/pki/tls/private/
-chmod 600 /var/lib/pgsql/data/server.key # /etc/pki/tls/private/server.key
-#update-ca-trust enable
-#update-ca-trust extract
-
-echo "Show files in data dir, should be server.crt in here..."
-ls -la /var/lib/pgsql/data
 
 echo "hostssl all         shah    0.0.0.0/0             md5 clientcert=1" >> /var/lib/pgsql/data/pg_hba.conf
