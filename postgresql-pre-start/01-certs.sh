a9fbb24a7ff8f29b7f7e5b3384e697a3a2db6c44
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-14 17:37:52 -0400

    Update 01-certs.sh

--- postgresql-pre-start/01-certs.sh
+++ postgresql-pre-start/01-certs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cp /tmp/certs/root.crt /var/lib/pgsql/data/root.crt
-sudo cp /tmp/certs/root.crt /etc/pki/ca-trust/source/anchors/
+# cp /tmp/certs/root.crt /etc/pki/ca-trust/source/anchors/
 cp /tmp/certs/server-cert.pem /var/lib/pgsql/data/server.crt
-sudo cp /tmp/certs/server-cert.pem /etc/pki/tls/certs/
+# cp /tmp/certs/server-cert.pem /etc/pki/tls/certs/
 cp /tmp/certs/server-key.pem /var/lib/pgsql/data/server.key
-sudo cp /tmp/certs/server-key.pem /etc/pki/tls/private/
-sudo chmod 600 /var/lib/pgsql/data/server.key /etc/pki/tls/private/server.key
-update-ca-trust enable
-update-ca-trust extract
+# cp /tmp/certs/server-key.pem /etc/pki/tls/private/
+chmod 600 /var/lib/pgsql/data/server.key # /etc/pki/tls/private/server.key
+#update-ca-trust enable
+#update-ca-trust extract
 
 echo "hostssl all         shah    0.0.0.0/0             md5 clientcert=1" >> /var/lib/pgsql/data/pg_hba.conf
