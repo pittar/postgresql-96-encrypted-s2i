54c78c4bc160fe2786fd9129884e4a0e55316bcb
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-14 22:04:14 -0400

    Debugging init and pre-start scripts.

---
+++ postgresql-init/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch /var/lib/pgsql/data/init.txt
+
+echo "Listing data dir..."
+
+ls -la /var/lib/pgsql/data
---
+++ postgresql-pre-start/pre-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch /var/lib/pgsql/data/pre-start.txt
+
+echo "Listing data dir..."
+
+ls -la /var/lib/pgsql/data
