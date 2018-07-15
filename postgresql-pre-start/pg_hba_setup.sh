58abfcc651697dad8f2765e2a1ba78fb1976c540
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-14 23:02:57 -0400

    Echo certs dir.

--- postgresql-pre-start/pg_hba_setup.sh
+++ postgresql-pre-start/pg_hba_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "What is in the certs dir."
+
+ls -la /var/lib/pgsql/certs
 
 echo "hostssl all         shah    0.0.0.0/0             md5 clientcert=1" >> /var/lib/pgsql/data/pg_hba.conf
