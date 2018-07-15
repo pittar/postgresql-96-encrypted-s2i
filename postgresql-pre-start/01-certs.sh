c1440d7e8bc9012507884f590aa90bea104c11e9
Andrew Pitt <andrew.pitt@canada.ca>
2018-07-14 22:26:45 -0400

    SSL.

---
+++ postgresql-pre-start/01-certs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp /tmp/certs/root.crt /var/lib/pgsql/data/userdata/root.crt
+# cp /tmp/certs/root.crt /etc/pki/ca-trust/source/anchors/
+cp /tmp/certs/server.crt /var/lib/pgsql/data/userdata/server.crt
+#cp /tmp/certs/server-cert.pem /var/lib/pgsql/data/server.crt
+# cp /tmp/certs/server-cert.pem /etc/pki/tls/certs/
+cp /tmp/certs/server.key /var/lib/pgsql/data/userdata/server.key
+#cp /tmp/certs/server-key.pem /var/lib/pgsql/data/server.key
+# cp /tmp/certs/server-key.pem /etc/pki/tls/private/
+chmod 600 /var/lib/pgsql/data/userdata/server.key # /etc/pki/tls/private/server.key
+#update-ca-trust enable
+#update-ca-trust extract
+
+echo "Show files in data dir, should be server.crt in here..."
+ls -la /var/lib/pgsql/data/userdata
+
